dbc7c82b41e8700616884b920f507de0cf5bb30f
Ryan Dhungel <kaloraat@gmail.com>
2018-04-10 23:33:10 +1000

    first commit

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import Loading from './Loading';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // state
+    this.state = {
+      users: [],
+      loading: false
+    };
+    // bind
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  getUsers() {
+    this.setState({
+      loading: true
+    });
+    axios('https://api.randomuser.me/?nat=US&results=5').then(response =>
+      this.setState({
+        users: [...this.state.users, ...response.data.results],
+        loading: false
+      })
+    );
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    this.getUsers();
+    console.log('more users loaded');
+  }
+
+  componentWillMount() {
+    this.getUsers();
+  }
+
+  render() {
+    const { loading, users } = this.state;
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <input type="submit" value="load users" />
+        </form>
+        <hr />
+        {!loading ? (
+          users.map(user => (
+            <div key={user.id.value}>
+              <h3 style={{ color: 'red' }}>{user.name.first}</h3>
+              <p>{user.email}</p>
+              <hr />
+            </div>
+          ))
+        ) : (
+          <Loading message="Hey hey hey" />
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
